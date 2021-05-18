fec285163be03ba05da6ef738aad09f2b00321c6
Cloud11665 <Cloud11665@gmail.com>
2021-05-18 14:29:57 +0200

    add gzip middleware remove unused routers, allow starting by running the running the file directly

--- app.py
+++ app.py
@@ -1,20 +1,38 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
 
 from vlo import router as vlo
-from hwinfo import router as hwinfo
-from articles import router as articles
 
 app = FastAPI()
 
 app.add_middleware(
-    CORSMiddleware,
-    allow_credentials=True,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
+	CORSMiddleware,
+	allow_credentials=True,
+	allow_origins=["*"],
+	allow_methods=["*"],
+	allow_headers=["*"],
+)
+app.add_middleware(
+	GZipMiddleware,
+	minimum_size=1000,
 )
 
 app.include_router(vlo)
-app.include_router(articles)
-app.include_router(hwinfo)
+
+
+if __name__ == "__main__":
+	if __import__("os").system("grep -i microsoft /proc/version") == 0:
+		exit("Run it using a real operating system.")
+
+	pid = None
+	while pid is None:
+		__import__("os").system("2>/dev/null 1>&2 memcached &")
+		pid = __import__("subprocess").check_output("ps -i 'cmd,pid' | grep '^memcached' | awk '{print $2}'", shell=True)\
+					.decode()\
+					.strip()
+		pid = int(pid) if pid.isdigit() else None
+
+	__import__("uvicorn").run("app:app", port=7001, reload=True, log_level="debug")
+	print(f"Killing memcached process [{pid}]")
+	__import__("os").system(f"kill -9 {pid}")
